70ab83a21058b41b08becfdc3d44cd4689980d98
VadzimPapkou <playvarian1@gmail.com>
2025-12-04 14:47:12 +0300

    fix sort direction

--- src/features/search-form/construct-url.ts
+++ src/features/search-form/construct-url.ts
@@ -4,25 +4,21 @@ export const constructAniDBUrl = (formState: TSearchFormState): string => {
     const baseUrl = 'https://anidb.net/anime/';
     const params = new URLSearchParams();
 
-    // Параметры по умолчанию
     params.append('noalias', '1');
     params.append('do.update', 'Search');
 
-    // Эпизоды
     if (formState.episodesMin) {
         params.append('eps.min', formState.episodesMin.toString());
     }
 
-    // Годы выпуска
     if (formState.startYear && formState.startYear.length === 2) {
         const [start, end] = formState.startYear;
         params.append('airdate.start', `${start.year()}-?-?`);
         params.append('airdate.end', `${end.year()}-?-?`);
     }
 
-    // Сортировка
     if (formState.sortType) {
-        const direction = formState.sortDirection === 'desc' ? '0.1' : '0.2';
+        const direction = formState.sortDirection === 'desc' ? '0.2' : '0.1';
 
         if (formState.sortType === 'rating') {
             params.append('orderby.rating', direction);
