9dfc8249d9d0adb530a64e719dad3eeff60abb98
VadzimPapkou <playvarian1@gmail.com>
2025-12-09 01:02:04 +0300

    change font to montserrat

--- src/app/antd-config-provider.tsx
+++ src/app/antd-config-provider.tsx
@@ -8,7 +8,26 @@ type TAntdConfigProviderProps = {
 
 export function AntdConfigProvider({children}: TAntdConfigProviderProps) {
     return (
-        <ConfigProvider locale={ ruLocale }>
+        <ConfigProvider locale={ ruLocale } theme={ {
+            token: {
+                fontFamily: 'Montserrat',
+                fontSizeHeading1: 32,
+                fontSizeHeading2: 24,
+                fontSizeHeading3: 20,
+                fontSizeHeading4: 16,
+                fontSizeHeading5: 14,
+            },
+            components: {
+                Typography: {
+                    lineHeightHeading1: 1.2,
+                    lineHeightHeading2: 1.2,
+                    lineHeightHeading3: 1.2,
+                    lineHeightHeading4: 1.2,
+                    lineHeightHeading5: 1.2,
+                },
+            }
+
+        } }>
             { children }
         </ConfigProvider>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AntdConfigProvider } from './app/antd-config-provider.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import 'material-icons/iconfont/material-icons.css';
 import './shared/antd/ant-global.scss';
+import './shared/fonts.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
