573e1441b26fc3e2eeac10a37fcf8cffa425f75c
VadzimPapkou <playvarian1@gmail.com>
2025-12-12 05:35:12 +0300

    move fonts

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/app.tsx'
-import { queryClient } from "./shared/api/query-client.ts";
-import { AntdConfigProvider } from './app/antd-config-provider.tsx';
+import App from './app.tsx'
+import { queryClient } from "../shared/api/query-client.ts";
+import { AntdConfigProvider } from './antd-config-provider.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import 'material-icons/iconfont/material-icons.css';
-import './shared/antd/ant-global.scss';
-import './shared/fonts.css';
+import '../shared/antd/ant-global.scss';
+import '../shared/global.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
